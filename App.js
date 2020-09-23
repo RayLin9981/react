2c3408e864380e73fe31cebc50bd42949194f9d1
RayLin9981 <a259108736@gmail.com>
2020-09-23 11:42:26 +0800

    ssosos

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ export default function App() {
 
     <View style={styles.container}>
 
-      <Text>Hello</Text>
+      <Text>Helloooooo</Text>
 
       <Click/>
 
@@ -32,7 +32,7 @@ const styles = StyleSheet.create({
 
     flex: 1,
 
-    backgroundColor: '#fff',
+    backgroundColor: '#fff111',
 
     alignItems: 'center',
 
--- click.js
+++ click.js
@@ -1,29 +1,35 @@
-import React from 'react';
-
-import {Alert, Button} from 'react-native';
-
-
-
+import React, {useState , useEffect } from 'react';
+import {Alert ,Button} from 'react-native';
 export default function Click() {
 
-  let count = 0;
 
-  let countString = "count:"+count;
 
-  function handleClick() {
+const [count, setCount] = useState(0);
+
+let countString = "count:"+count;
+
+
+
+function handleClick() {
+
+ setCount(count+1);
+
+ Alert.alert("count:"+count);
+
+}
+function showCount(){
 
     Alert.alert("count:"+count);
 
-    count++;
-
   }
+useEffect(showCount);
 
-  return (
 
-    <Button title={countString} onPress={handleClick}/>
 
-   
+return (
 
-  );
+  <Button title={countString} onPress={handleClick}/>
+
+);
 
 }
