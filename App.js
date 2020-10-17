097f2df4b894fe00e20bb7baa9de5b3aea03c242
RayLin9981 <a259108736@gmail.com>
2020-10-17 09:20:55 +0800

    change to router

--- App.js
+++ App.js
@@ -1,28 +1,65 @@
-import React from 'react';
 
-import { StyleSheet, Text, View} from 'react-native';
+//加上另一個Screen
+
+import * as React from 'react';
+
+import { View, Text,Button } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+
+import { createStackNavigator } from '@react-navigation/stack';
 
 
 
-import Click from '/Users/yuzhin/test-reactnative/click.js';
-
-import ProductList from './src/product/ProductList.js';
-
-import ProductAdd from './src/product/ProductAdd.js';
-
-export default function App() {
+function HomeScreen({navigation}) {//利用大括號的裡面去抓原本props裡面給的名稱變數
 
   return (
-  
 
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
 
-    <View style={styles.container}>
-
-      <Text>Hello</Text>
-
-      <Click count={0}/>
-      <ProductList/>
+      <Text>Home Screen</Text>
+<Button title = "good" onPress = { () => navigation.navigate('Details')}></Button>
     </View>
+
+  );
+
+}
+
+function DetailsScreen(navigation) {
+
+    return (
+
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+
+        <Text>Details Screen</Text>
+
+      </View>
+
+    );
+
+  }
+
+
+
+const Stack = createStackNavigator();
+
+
+
+function App() {
+
+  return (
+
+    <NavigationContainer>
+
+      <Stack.Navigator>
+
+        <Stack.Screen name="Home123" component={HomeScreen} />
+
+        <Stack.Screen name="Details" component={DetailsScreen} />
+
+      </Stack.Navigator>
+
+    </NavigationContainer>
 
   );
 
@@ -30,19 +67,4 @@ export default function App() {
 
 
 
-const styles = StyleSheet.create({
-
-  container: {
-
-    flex: 1,
-
-    backgroundColor: '#ffffff',
-
-    alignItems: 'center',
-
-    justifyContent: 'center',
-
-  },
-
-});
-
+export default App;
