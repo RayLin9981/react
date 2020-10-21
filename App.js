59fb64ddeaabd09ee2682701b54e4356af555383
RayLin9981 <a259108736@gmail.com>
2020-10-21 10:41:13 +0800

    thit great

--- App.js
+++ App.js
@@ -1,37 +1,87 @@
-let getData = ()=> {
+import React from 'react';
 
-  return new Promise((resolve,reject)=> {
-
-    //模擬要花時間取得資料
-
-    setTimeout(
-
-      //()=>reject('Error')
-
-      ()=>resolve([{name:'Ben', age:30},{name:'Mary', age:20}])
-
-      , 3000);
-
-  });
-
-}
+import { StyleSheet, Text, View} from 'react-native';
 
 
 
-const getRemoteData = async()=> {
+import Click from '/Users/yuzhin/test-reactnative/click.js';
 
-  result = await getData();
+import ProductList from './src/product/ProductList.js';
 
+import ProductAdd from './src/product/ProductAdd.js';
+import ProductList_pra from './src/product/ProductList_pra.js';
+
+export default function App() {
+
+  let getData = ()=> {
+
+    return new Promise((resolve,reject)=> {
+  
+      //模擬要花時間取得資料
+  
+      setTimeout(
+  
+        //()=>reject('Error')
+  
+        ()=>resolve([{name:'Ben', age:30},{name:'Mary', age:20}])
+  
+        , 3000);
+  
+    });
+  
+  }
+  
+  
+  
+  const getRemoteData = async()=> {
+  
+    result = await getData();
+  
+    console.log(result);
+  
+  }
+  
+  
+  
+  console.log("start");
+  
+  let result = getRemoteData();
+  
   console.log(result);
+  
+  console.log("end");
+  return (
+  
+
+
+    <View style={styles.container}>
+
+    <Text>Hello</Text>
+
+      <Click count={0}/>
+      <ProductList_pra></ProductList_pra>
+      <ProductList/>
+    </View>
+
+  );
 
 }
 
 
 
-console.log("start");
+const styles = StyleSheet.create({
 
-result = getRemoteData();
+  container: {
 
-console.log(result);
+    flex: 1,
 
-console.log("end");
+    backgroundColor: '#FFC1E0',
+
+    alignItems: 'center',
+
+    justifyContent: 'center',
+
+  },
+
+});
+
